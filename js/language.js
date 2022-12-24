94d195b94018b2fb22e613e3e4f4f0a7763024c6
pb-4 <balabanovic.paja@gmail.com>
2022-12-24 17:09:16 +0100

    added language(eng/ger/fra)

--- js/language.js
+++ js/language.js
@@ -1,4 +1,5 @@
 const languageData = [
+    // SRB
   {
     nav: [
       { href: "#offer", text: "Šta vam nudimo?" },
@@ -50,8 +51,158 @@ const languageData = [
     },
   },
   // ENG
-  // GER
+  {
+    nav: [
+      { href: "#offer", text: "What we offer?" },
+      { href: "#team", text: "Who we are?" },
+      { href: "#portfolio", text: "Our portfolio" },
+      { href: "#contact", text: "Contact" },
+    ],
+    hero: "Design and development agency<br />for the<span> digital age</span>",
+    button: { contact: "Contact", send: "Send" },
+    offer: {
+      title: "What we offer?",
+      points: [
+        {
+          img: "images/brush.svg",
+          text: "Website design, development and development",
+        },
+        {
+          img: "images/seo.svg",
+          text: "SEO Optimization",
+        },
+        {
+          img: "images/lock.svg",
+          text: "Data encryption and security",
+        },
+        {
+          img: "images/hp.svg",
+          text: "24/7 support",
+        },
+      ],
+    },
+    team: {
+      title: "Who we are?",
+      memberDesc: {
+        milan:
+          "Milan is studying Computer Science at the Faculty of Mathematics in Novi Sad. He is a full stack web developer who has always loved programming. As a young man, he started learning programming and web application development technologies. Although he is young, he participated in several competitions that he won with the team and worked with the Vega IT company on the project.",
+        nikola:
+          "Nikola is a graphic designer with a focus on UI/UX. He graduated from an electrical engineering school and has since started designing websites. He is constantly improving in the use of front-end technologies such as HTML, CSS and JavaScript. He has a talent for understanding user needs and creating designs that are easy to use and pleasing to look at.",
+        pavle:
+          "Pavle is a medical student who has a love for IT. He is a front-end developer who creates and maintains websites. He is an expert in using HTML, CSS and JavaScript to create attractive and functional web pages. He loves working with his clients to help them achieve their goals and achieve success online.",
+        mateja:
+          "Mateja is an extroverted and sociable student of hotel management who deals with marketing and content writing. Mateja has a talent for writing and enjoys developing different types of content, from short ads to long articles. He is dedicated to working with his clients and always strives to provide them with the best possible service.",
+      },
+    },
+    portfolio: {
+      title: "Our portfolio",
+    },
+    contact: {
+      title: "Contact us",
+    },
+  },
+  // Deutsch
+  {
+    nav: [
+      { href: "#offer", text: "Was bieten wir Ihnen?" },
+      { href: "#team", text: "Wer sind wir?" },
+      { href: "#portfolio", text: "Unser Portfolio" },
+      { href: "#contact", text: "Kontakt" },
+    ],
+    hero: "Agentur für Design und Entwicklung<br />Für <span> das digitale Zeitalter</span>",
+    button: { contact: "Kontakt", send: "Schicken Sie es" },
+    offer: {
+      title: "Was bieten wir Ihnen?",
+      points: [
+        {
+          img: "images/brush.svg",
+          text: "Website Design, Entwicklung und Entwicklung",
+        },
+        {
+          img: "images/seo.svg",
+          text: "SEO Optimierung",
+        },
+        {
+          img: "images/lock.svg",
+          text: "Datenverschlüsselung und Sicherheit",
+        },
+        {
+          img: "images/hp.svg",
+          text: "24/7 Support",
+        },
+      ],
+    },
+    team: {
+      title: "Wer sind wir?",
+      memberDesc: {
+        milan:
+          "Milan studiert Informatik an der Fakultät für Mathematik in Novi Sad. Er ist ein Full-Stack-Webentwickler, der das Programmieren schon immer geliebt hat. Als junger Mann begann er mit dem Erlernen von Programmier- und Webanwendungsentwicklungstechnologien. Obwohl er jung ist, nahm er an mehreren Wettbewerben teil, die er mit dem Team gewann, und arbeitete mit der Firma Vega IT an dem Projekt.",
+        nikola:
+          "Nikola ist Grafikdesigner mit Schwerpunkt UI/UX. Er absolvierte eine Schule für Elektrotechnik und hat seitdem mit der Gestaltung von Websites begonnen. Er verbessert ständig den Einsatz von Frontend-Technologien wie HTML, CSS und JavaScript. Er hat ein Händchen dafür, die Bedürfnisse der Benutzer zu verstehen und Designs zu erstellen, die einfach zu bedienen und ansprechend anzusehen sind.",
+        pavle:
+          "Pavle ist ein Medizinstudent mit einer Vorliebe für IT. Er ist ein Frontend-Entwickler, der Websites erstellt und pflegt. Er ist Experte für die Verwendung von HTML, CSS und JavaScript zur Erstellung attraktiver und funktionaler Webseiten und liebt es, mit seinen Kunden zusammenzuarbeiten, um ihnen dabei zu helfen, ihre Ziele zu erreichen und online erfolgreich zu sein..",
+        mateja:
+          "Mateja ist eine extrovertierte und gesellige Studentin des Hotelmanagements, die sich mit Marketing und Content Writing beschäftigt. Mateja hat ein Talent zum Schreiben und entwickelt gerne verschiedene Arten von Inhalten, von kurzen Anzeigen bis hin zu langen Artikeln. Er widmet sich der Arbeit mit seinen Kunden und ist stets bestrebt, ihnen den bestmöglichen Service zu bieten.",
+      },
+    },
+    portfolio: {
+      title: "Unser Portfolio",
+    },
+    contact: {
+      title: "Kontaktiere uns",
+    },
+  },
   // FRA
+  {
+    nav: [
+      { href: "#offer", text: "Ce que nous vous proposons?" },
+      { href: "#team", text: "Qui sommes nous?" },
+      { href: "#portfolio", text: "Notre portfolio" },
+      { href: "#contact", text: "Contacter" },
+    ],
+    hero: "Agence de conception et développement<br />Pour <span> l'ère numérique</span>",
+    button: { contact: "Contacter", send: "Envoie le" },
+    offer: {
+      title: "Ce que nous vous proposons?",
+      points: [
+        {
+          img: "images/brush.svg",
+          text: "Conception, développement et développement de sites Web",
+        },
+        {
+          img: "images/seo.svg",
+          text: "Optimisation du référencement",
+        },
+        {
+          img: "images/lock.svg",
+          text: "Cryptage et sécurité des données",
+        },
+        {
+          img: "images/hp.svg",
+          text: "Assistance 24/7",
+        },
+      ],
+    },
+    team: {
+      title: "Qui sommes nous?",
+      memberDesc: {
+        milan:
+          "Milan étudie l'informatique à la faculté de mathématiques de Novi Sad. C'est un développeur web full stack qui a toujours aimé la programmation. Jeune homme, il a commencé à apprendre la programmation et les technologies de développement d'applications Web. Bien qu'il soit jeune, il a participé à plusieurs concours qu'il a remportés avec l'équipe et a travaillé avec la société Vega IT sur le projet.",
+        nikola:
+          "Nikola est un graphiste spécialisé dans l'UI/UX. Il est diplômé d'une école de génie électrique et a depuis commencé à concevoir des sites Web. Il s'améliore constamment dans l'utilisation des technologies frontales telles que HTML, CSS et JavaScript. Il a le don de comprendre les besoins des utilisateurs et de créer des designs faciles à utiliser et agréables à regarder.",
+        pavle:
+          "Pavle est un étudiant en médecine passionné par l'informatique. Il est un développeur front-end qui crée et maintient des sites Web. Il est un expert dans l'utilisation de HTML, CSS et JavaScript pour créer des pages Web attrayantes et fonctionnelles. Il aime travailler avec ses clients pour les aider à atteindre leurs objectifs et à réussir en ligne.",
+        mateja:
+          "Mateja est une étudiante extravertie et sociable en gestion hôtelière qui s'occupe du marketing et de la rédaction de contenu. Mateja a un talent pour l'écriture et aime développer différents types de contenu, des petites annonces aux longs articles. Il est dévoué à travailler avec ses clients et s'efforce toujours de leur fournir le meilleur service possible.",
+      },
+    },
+    portfolio: {
+      title: "Notre portfolio",
+    },
+    contact: {
+      title: "Nous contacter",
+    },
+  },
 ];
 
 function applyLanguage(lang) {
